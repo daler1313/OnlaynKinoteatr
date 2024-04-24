d43463f5c5e84ffff20ed6378ffa6e15968e68c8
daler1313 <dfozilov031@gmail.com>
2024-04-24 18:36:41 +0500

    UpdateUserRequest

---
+++ Contracts/Requests/GetAllUserRequest.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Contracts.Requests.User
+{
+    public class GetAllUserRequest 
+    {
+        public IEnumerable<Domain.Entities.User> Items { get; set; } = Enumerable.Empty<Domain.Entities.User>();
+    }
+
+}
